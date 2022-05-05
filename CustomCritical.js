3d22edc535c152f72a212c92c0929a9a54103183
JamesBrandts <91541345+JamesBrandts@users.noreply.github.com>
2022-05-04 22:45:34 -0300

    Update CustomCritical.js

--- CustomCritical.js
+++ CustomCritical.js
@@ -2,42 +2,10 @@ Hooks.on("midi-qol.preAttackRoll", async (workflow) =>{
     const attacker = game.actors.get(workflow.actor.id)
     const targetAc = canvas.tokens.get(workflow.targets.ids[0]).actor.data.data.attributes.ac.value
     const attackBonus = workflow.item.labels.toHit
-    const newThreshold = targetAc + 10 - eval('0' + attackBonus)
-    if(newThreshold < 20){
-        let effectData = {
-            "flags": {
-                "dae": {
-                    "transfer": false,
-                    "stackable": "none",
-                    "macroRepeat": "none",
-                    "specialDuration": ["1Attack"]
-                }
-            },
-            "changes": [
-                {
-                    "key": "flags.dnd5e.weaponCriticalThreshold",
-                    "mode": 5,
-                    "value": newThreshold,
-                    "priority": "20"
-                },
-                {
-                    "key": "flags.dnd5e.spellCriticalThreshold",
-                    "mode": 5,
-                    "value": newThreshold,
-                    "priority": "20"
-                }
-            ],
-            "disabled": false,
-            "duration": {
-                "startTime": null,
-                "turns":1
-            },
-            "icon": "systems/dnd5e/icons/skills/red_10.jpg",
-            "label": "Encrised Critical",
-            "tint": null,
-            "transfer": false,
-        }
-        await attacker.createEmbeddedDocuments('ActiveEffect',[effectData])
-        await new Promise(r => setTimeout(r, 200));
-    }    
+    const calc = targetAc + 10 - eval('0' + attackBonus)
+    let newThreshold = 20
+    if (calc<20){newThreshold = calc}
+    await attacker.setFlag('dnd5e','weaponCriticalThreshold',newThreshold)
+    await attacker.setFlag('dnd5e','spellCriticalThreshold',newThreshold)
+    await new Promise(r => setTimeout(r, 200));  
 })
