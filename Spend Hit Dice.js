276b551ff545f8e844cbe426cf0fed12fa937213
JamesBrandts <91541345+JamesBrandts@users.noreply.github.com>
2022-05-31 11:58:17 -0300

    Update Spend Hit Dice.js

--- Spend Hit Dice.js
+++ Spend Hit Dice.js
@@ -1,69 +1,94 @@
-if(args[0] == "off"){
-    let actorA = game.actors.get(args[2].actorId)
-    let hdAll = [0,0,0,0]
-    let hd6 = actorA.data.items.filter(i=>i.data.data.hitDice == "d6")
-    for (let hdClass of hd6){
-        hdAll[0] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed
-    }
-    let hd8 = actorA.data.items.filter(i=>i.data.data.hitDice == "d8")
-    for (let hdClass of hd8){
-        hdAll[1] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed
-    }
-    let hd10 = actorA.data.items.filter(i=>i.data.data.hitDice == "d10")
-    for (let hdClass of hd10){
-        hdAll[2] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed
-    }
-    let hd12 = actorA.data.items.filter(i=>i.data.data.hitDice == "d12")
-    for (let hdClass of hd12){
-        hdAll[3] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed
-    }
-    let hdOptions = ''
-    let options = []
-    if(hdAll[0]>0){
-        hdOptions += '<option value = d6> d6 (' + hdAll[0] + ' Available)</option>'
-        options.push('d6')
-    }
-    if(hdAll[1]>0){
-        hdOptions += '<option value = d8> d8 (' + hdAll[1] + ' Available)</option>'
-        options.push('d8')
-    }
-    if(hdAll[2]>0){
-        hdOptions += '<option value = d10> d10 (' + hdAll[2] + ' Available)</option>'
-        options.push('d10')
-    }
-    if(hdAll[3]>0){
-        hdOptions += '<option value = d12> d12 (' + hdAll[3] + ' Available)</option>'
-        options.push('d12')
-    }
-
-    if(options.length > 1){
-        let opt = '<div class="form-group"><label for="exampleSelect">Select spell slot</label><select name="exampleSelect">' + hdOptions + '</select></div>'
-
-        let d = await new Dialog({
-            title: 'Create sorcery points',
-            content: opt,
-            buttons: {
-            yes: {
-                icon: '<i class="fas fa-check"></i>',
-                label: 'OK',
-                callback: (html) => {
-                let select = html.find('[name="exampleSelect"]').val();
-                healActor(actorA,select)    
-                },
-            },
-        }}
-        ).render(true)
-    }
-    if(options.length == 1){
-        healActor(actorA,options[0]) 
-    }
+//This is prepared to be used as an Item Macro;
+const lastArg = args[args.length-1];
+const actorA = game.actors.get(lastArg.actorId);
+//If you want to use this as a raw macro, in the macro bar, to use by the selected token comment the 2 previous lines and uncomment the next one
+//const actorA = canvas.tokens.controlled[0].actor
+let hd = retrieveHD(actorA);
+let hdTotal = 0
+for(let hDiceCount of hd){
+    hdTotal += hDiceCount
 }
-async function healActor(actorA,dice){
-    let diceH = dice + '+' + actorA.data.data.abilities.con.mod
-    let healRoll = await new Roll(diceH).evaluate()
-    healRoll.toMessage()
-    let heal = healRoll.total
-    actorA.applyDamage(-heal)
-    let spent = actorA.data.items.find(i=>i.data.data.hitDice == dice && i.data.data.levels > i.data.data.hitDiceUsed)
-    spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})    
+let hdOptions = '';
+for(let i = 1;i<=hdTotal;i++){
+    hdOptions += `<option value = ${i}>${i}</option>`;
+}
+let opt = `<div class="form-group"><label for="exampleSelect">Quantity: </label><select name="exampleSelect">${hdOptions}</select></div>`
+let d = await new Dialog({
+    title: 'Spend Hit Dice for Elemental Surge Point',
+    content: opt,
+    buttons: {
+    yes: {
+        icon: '<i class="fas fa-check"></i>',
+        label: 'OK',
+        callback: (html) => {
+        let select = html.find('[name="exampleSelect"]').val();
+        surgePoints(actorA,select)    
+        },
+    },
+}}
+).render(true)
+async function retrieveHD(actorA){    
+    let hdAll = [0,0,0,0];
+    let hd6 = actorA.data.items.filter(i=>i.data.data.hitDice == "d6");
+    for (let hdClass of hd6){
+        hdAll[0] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed;
+    }
+    let hd8 = actorA.data.items.filter(i=>i.data.data.hitDice == "d8");
+    for (let hdClass of hd8){
+        hdAll[1] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed;
+    }
+    let hd10 = actorA.data.items.filter(i=>i.data.data.hitDice == "d10");
+    for (let hdClass of hd10){
+        hdAll[2] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed;
+    }
+    let hd12 = actorA.data.items.filter(i=>i.data.data.hitDice == "d12");
+    for (let hdClass of hd12){
+        hdAll[3] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed;
+    }
+    return hdAll;
+}    
+
+async function surgePoints(actorA,points){
+    for(let i = 0;i<points;i++){
+        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d6" && i.data.data.levels > i.data.data.hitDiceUsed)){
+            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
+            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
+            if(i>1){
+                    let damageRoll = await new Roll("d6").evaluate()
+                    damageRoll.toMessage()
+                    let damage = damageRoll.total
+                    actorA.applyDamage(damage)
+            }
+        }
+        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d8" && i.data.data.levels > i.data.data.hitDiceUsed)){
+            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
+            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
+            if(i>1){
+                    let damageRoll = await new Roll("d8").evaluate()
+                    damageRoll.toMessage()
+                    let damage = damageRoll.total
+                    actorA.applyDamage(damage)
+            }
+        }
+        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d10" && i.data.data.levels > i.data.data.hitDiceUsed)){
+            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
+            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
+            if(i>1){
+                    let damageRoll = await new Roll("d10").evaluate()
+                    damageRoll.toMessage()
+                    let damage = damageRoll.total
+                    actorA.applyDamage(damage)
+            }
+        }
+        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d12" && i.data.data.levels > i.data.data.hitDiceUsed)){
+            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
+            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
+            if(i>1){
+                    let damageRoll = await new Roll("d12").evaluate()
+                    damageRoll.toMessage()
+                    let damage = damageRoll.total
+                    actorA.applyDamage(damage)
+            }
+        }
+    }
 }
