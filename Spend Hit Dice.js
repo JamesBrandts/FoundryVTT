e56a6f0ec96105b9b2f4020de64f0542fc8d3dc0
JamesBrandts <91541345+JamesBrandts@users.noreply.github.com>
2022-05-31 21:14:17 -0300

    Update Spend Hit Dice.js

--- Spend Hit Dice.js
+++ Spend Hit Dice.js
@@ -1,12 +1,16 @@
 //This is prepared to be used as an Item Macro;
 const lastArg = args[args.length-1];
-const actorA = game.actors.get(lastArg.actorId);
+const actorA = game.actors.get(lastArg.actor._id)
 //If you want to use this as a raw macro, in the macro bar, to use by the selected token comment the 2 previous lines and uncomment the next one
 //const actorA = canvas.tokens.controlled[0].actor
-let hd = retrieveHD(actorA);
+let hd = await retrieveHD(actorA);
 let hdTotal = 0
 for(let hDiceCount of hd){
     hdTotal += hDiceCount
+}
+if(hdTotal<1){
+    ui.notifications.warn("No hit dice to spend");
+    return
 }
 let hdOptions = '';
 for(let i = 1;i<=hdTotal;i++){
@@ -46,49 +50,34 @@ async function retrieveHD(actorA){
         hdAll[3] += hdClass.data.data.levels - hdClass.data.data.hitDiceUsed;
     }
     return hdAll;
-}    
-
+}
 async function surgePoints(actorA,points){
-    for(let i = 0;i<points;i++){
-        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d6" && i.data.data.levels > i.data.data.hitDiceUsed)){
-            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
-            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
-            if(i>1){
-                    let damageRoll = await new Roll("d6").evaluate()
-                    damageRoll.toMessage()
-                    let damage = damageRoll.total
-                    actorA.applyDamage(damage)
-            }
-        }
-        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d8" && i.data.data.levels > i.data.data.hitDiceUsed)){
-            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
-            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
-            if(i>1){
-                    let damageRoll = await new Roll("d8").evaluate()
-                    damageRoll.toMessage()
-                    let damage = damageRoll.total
-                    actorA.applyDamage(damage)
-            }
-        }
-        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d10" && i.data.data.levels > i.data.data.hitDiceUsed)){
-            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
-            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
-            if(i>1){
-                    let damageRoll = await new Roll("d10").evaluate()
-                    damageRoll.toMessage()
-                    let damage = damageRoll.total
-                    actorA.applyDamage(damage)
-            }
-        }
-        if(spent = actorA.data.items.find(i=>i.data.data.hitDice == "d12" && i.data.data.levels > i.data.data.hitDiceUsed)){
-            spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
-            actorA.update({'resources.tertiary.value':actorA.data.resources.tertiary.value + 1})
-            if(i>1){
-                    let damageRoll = await new Roll("d12").evaluate()
-                    damageRoll.toMessage()
-                    let damage = damageRoll.total
-                    actorA.applyDamage(damage)
-            }
-        }
+    let i = 0
+    while(i < points && await spendDice(actorA,"d6",i)){     
+        i++
     }
+    while(i < points && await spendDice(actorA,"d8",i)){
+        i++
+    }
+    while(i < points && await spendDice(actorA,"d10",i)){
+        i++
+    }
+    while(i < points && await spendDice(actorA,"d12",i)){
+        i++
+    }   
+}
+
+async function spendDice(actorA,dice,i){
+    if(spent = actorA.data.items.find(j=>j.data.data.hitDice == dice && j.data.data.levels > j.data.data.hitDiceUsed)){
+        await spent.update({'data.hitDiceUsed':spent.data.data.hitDiceUsed+1})
+        await actorA.update({'data.resources.tertiary.value':actorA.data.data.resources.tertiary.value + 1})
+        if(i>0){
+            let damageRoll = await new Roll(dice).evaluate()
+            damageRoll.toMessage()
+            let damage = damageRoll.total
+            actorA.applyDamage(damage)
+        }
+        return true;
+    }
+    return false
 }
